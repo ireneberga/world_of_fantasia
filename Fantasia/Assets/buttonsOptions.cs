967862ec4bb1c13ea16132f7fd0b157aba6c09ee
ireneberga <145671564+ireneberga@users.noreply.github.com>
2023-12-06 17:09:50 +0100

    Aggiunta pulsanti pause, restart, next

---
+++ Fantasia/Assets/buttonsOptions.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonsOptions : MonoBehaviour
+{
+    private bool isPausato = false;
+    
+    public void Termina()
+    {
+        SceneManager.LoadScene(4);
+    }
+    
+    public void Rincomincia()
+    {
+        SceneManager.LoadScene(4);
+    }
+    
+    public void TogglePausa()
+    {
+        if (isPausato)
+        {
+            RiprendiGioco();
+        }
+        else
+        {
+            MettiInPausa();
+        }
+    }
+
+    void MettiInPausa()
+    {
+        Time.timeScale = 0f; // Imposta la scala del tempo a 0 per mettere in pausa la scena
+        isPausato = true;
+    }
+
+    void RiprendiGioco()
+    {
+        Time.timeScale = 1f; // Ripristina la scala del tempo a 1 per riprendere la scena
+        isPausato = false;
+    }
+}
